3af48d8ffc4d384ca9e3cda84de9c7acaa8e5e22
Wojciech Przybylski <wojciech.przybylski1@hotmail.com>
2023-03-08 21:57:07 +0000

    fetch all products and add to view context

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from webapp.models import Product
 # Create your views here.
 
+
 def index(request):
-    return render(request, "index.html")
+    products = Product.objects.all()
+    return render(request, "index.html", {"products": products})
+
